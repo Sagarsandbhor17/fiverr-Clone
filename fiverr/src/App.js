eaa053b6bdcd7631ce07fb67fb65a6dddf247b2b
Sagarsandbhor17 <sagar12500@gmail.com>
2022-07-20 20:55:01 +0530

    day-2 work

---
+++ fiverr/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Fiverr from './Component/Fiverr';
+
+function App() {
+  return (
+    <div className="App">
+    <Fiverr/>   
+    </div>
+  );
+}
+
+export default App;
