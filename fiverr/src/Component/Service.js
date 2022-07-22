ca77c529c4690145e37a019618687c64db978558
Sagarsandbhor17 <sagar12500@gmail.com>
2022-07-22 23:00:28 +0530

    day-4 work

---
+++ fiverr/src/Component/Service.js
@@ -0,0 +1,52 @@
+
+export const service=
+[
+{p:"Build your brand",h:'Logo Design',img:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png'},
+{p:'Customize your site',h:'WordPress',img:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png'},
+{p:'Share your massage',h:'Voice Over',img:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png'},
+{p:'Engage your audience',h:'Video Explainer',img:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png'},
+{p:'Reach More Customer',h:'Social Media',img:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png'},
+{p:'Unlock growth online',h:'SEO',img:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741668/seo-2x.png'},
+{p:'Code your dreams',h:'Illustration',img:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741664/illustration-2x.png'},
+{p:'Go global',h:'Translation',img:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741674/translation-2x.png'},
+{p:'Learn your business',h:'Data Entry',img:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741664/data-entry-2x.png'},
+{p:'Showcase your story',h:'Book Covers',img:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/book-covers-2x.png'},
+]
+
+export const market=
+[
+{a:"https://www.fiverr.com/categories/graphics-design?source=hplo_cat_sec&pos=1",
+img:'https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/graphics-design.d32a2f8.svg',
+title:'Graphics & Design'},
+{a:"https://www.fiverr.com/categories/online-marketing?source=hplo_cat_sec&pos=2",
+img:'https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/online-marketing.74e221b.svg',
+title:'Digital Marketing'
+},
+{a:'https://www.fiverr.com/categories/writing-translation?source=hplo_cat_sec&pos=3',
+img:'https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/writing-translation.32ebe2e.svg',
+title:'writing & translation'
+},
+{a:'https://www.fiverr.com/categories/video-animation?source=hplo_cat_sec&pos=4',
+img:'https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/video-animation.f0d9d71.svg',
+title:'Video & Animation',
+},
+{a:'https://www.fiverr.com/categories/music-audio?source=hplo_cat_sec&pos=5',
+img:'https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/music-audio.320af20.svg',
+title:'Music & Audio'
+},
+{a:'https://www.fiverr.com/categories/programming-tech?source=hplo_cat_sec&pos=6',
+img:'https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/programming.9362366.svg',
+title:'Programming & Tech'
+},
+{a:'https://www.fiverr.com/categories/business?source=hplo_cat_sec&pos=7',
+img:'https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/business.bbdf319.svg',
+title:'Business'
+},
+{a:'https://www.fiverr.com/categories/lifestyle?source=hplo_cat_sec&pos=8',
+img:'https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/lifestyle.745b575.svg',
+title:'Lifestyle'
+},
+{a:'https://www.fiverr.com/categories/data?source=hplo_cat_sec&pos=9',
+img:'https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/data.718910f.svg',
+title:'Data'}
+]
